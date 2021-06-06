41588d17648857333b051a635f4ef8e34438a7e3
SoySauceGGD <75675203+SoySauceGGD@users.noreply.github.com>
2021-06-06 10:43:01 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include<queue>
 #include<set>
 #include<random>
-#include<ctime>
 #include<windows.h>
 #include<conio.h>
 
@@ -10,8 +9,8 @@ using namespace std;
 
 const int N = 30;
 const char WALL = '#';
-const char FOOD = '@';
-const char SNAKE_HEAD = '+';
+const char FOOD = '+';
+const char SNAKE_HEAD = '@';
 const char SNAKE_BODY = '*';
 
 typedef pair<int, int> pos; // x y coordinates
@@ -60,82 +59,79 @@ class Snake{
     private:
         queue<pos> snake;
         set<pos> snake_set;
-        Screen map;
+        Screen screen;
         random_device rd;
         char dire;
         pos food_pos;
+        int score;
 
     public:
         Snake(){
-            map.init();
-
+            screen.init();
             int x = N / 2, y = N / 2;
             for(int i = 0; i < 4; i++){
                 snake.push(make_pair(x + i, y));
                 snake_set.insert(make_pair(x + i, y));
-                map.setXYC(x + i, y, SNAKE_BODY);
+                screen.setXYC(x + i, y, SNAKE_BODY);
             }
-            map.setXYC(snake.back(), SNAKE_HEAD);
-            dire = 'W';
+            screen.setXYC(snake.back(), SNAKE_HEAD);
+            dire = 'S';
             food_pos = GenFood();
-            map.print();
-        }
-
-        bool oper(){
-            if(!move()){
-                return false;
-            }
-            map.print();
-            return true;
         }
 
         bool move(){
-            char in = getDire();
+            if(kbhit() != 0){
+                char tmp = toupper(getch());
+                if(dire != revDirection(tmp) && revDirection(tmp) != ' ')
+                    dire = tmp;
+            }
+
             pos old_head = snake.back(),
                 new_head = snake.back();
-            switch(in){
+            switch(dire){
                 case 'W':   case 'S':
-                    dire = (in == 'W' ? 'S' : 'W');
-                    new_head.first += (in == 'W' ? -1 : 1);
+                    new_head.first += (dire == 'W' ? -1 : 1);
                     break;
 
                 case 'A':   case 'D':
-                    dire = (in == 'A' ? 'D' : 'A');
-                    new_head.second += (in == 'A' ? -1 : 1);
+                    new_head.second += (dire == 'A' ? -1 : 1);
                     break;
             }
             if(!isAlive(new_head))
                 return false;
 
             if(new_head != food_pos){
-                map.setXYC(snake.front(), ' ');
+                screen.setXYC(snake.front(), ' ');
                 snake_set.erase(snake.front());
                 snake.pop();
             }else{
-                GenFood();
+                food_pos = GenFood();
+                score++;
             }
 
             snake.push(new_head);
             snake_set.insert(new_head);
-            map.setXYC(old_head, SNAKE_BODY);
-            map.setXYC(new_head, SNAKE_HEAD);
+            screen.setXYC(old_head, SNAKE_BODY);
+            screen.setXYC(new_head, SNAKE_HEAD);
+
+            screen.print();
+            cout << "  ! Press w/s/a/d to move\t! Score = " << score << endl;
+            Sleep(250);
             return true;
         }
 
-        char getDire(){
-            char in;
-            start:
-            if(kbhit() != 0){
-                in = toupper(_getch());
+        char revDirection(char c){
+            switch(c){
+                case 'W': return 'S';
+                case 'S': return 'W';
+                case 'A': return 'D';
+                case 'D': return 'A';
+                default : return ' ';
             }
-            if(in != dire && (in == 'W' || in == 'S' || in == 'A' || in == 'D'))
-                return in;
-            else
-                goto start;
         }
 
         bool isAlive(pos head){
-            return map.getXYChar(head) != SNAKE_BODY && map.getXYChar(head) != WALL;
+            return screen.getXYChar(head) != SNAKE_BODY && screen.getXYChar(head) != WALL;
         }
 
         pos GenFood(){
@@ -143,14 +139,13 @@ class Snake{
             auto it = snake_set.find(tmp);
             while(it != snake_set.end()){
                 tmp = gen();
+                it = snake_set.find(tmp);
             }
-            map.setXYC(tmp, FOOD);
-            food_pos = tmp;
+            screen.setXYC(tmp, FOOD);
             return tmp;
         }
-        
         pos gen(){
-            mt19937 generator(rd() + time(NULL));
+            mt19937 generator(rd());
             uniform_int_distribution<> uid(1, N-2);
             return make_pair(uid(generator), uid(generator));
         }
@@ -158,9 +153,10 @@ class Snake{
 
 int main(){
     ios::sync_with_stdio(false);
-    system("mode con cols=62 lines=31");
+    system("mode con cols=60 lines=34");
     Snake snake;
-    while(snake.oper());
+    while(snake.move());
+    
     system("cls");
     cout << "You Are Dead! ;(" << endl;
     system("PAUSE");
